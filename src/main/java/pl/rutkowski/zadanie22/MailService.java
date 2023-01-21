4c16eee564da99efecbe6c574b74b74e0611afb3
adrianrut <adrianrutkowskiarcgas@gmail.com>
2023-01-21 19:40:06 +0100

    Rozwiazanie

---
+++ src/main/java/pl/rutkowski/zadanie22/MailService.java
@@ -0,0 +1,24 @@
+package pl.rutkowski.zadanie22;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    private final JavaMailSender javaMailSender;
+
+    public MailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendMail(MailDto mailDto) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo("studiotest@op.pl");
+        mailMessage.setSubject(mailDto.getName());
+        mailMessage.setText(mailDto.getContent());
+        javaMailSender.send(mailMessage);
+    }
+
+}
---
+++ src/main/java/pl/rutkowski/zadanie22/StudioController.java
@@ -0,0 +1,36 @@
+package pl.rutkowski.zadanie22;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudioController {
+
+    private final MailService mailService;
+
+    public StudioController(MailService mailService) {
+        this.mailService = mailService;
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/offer")
+    public String offer() {
+        return "offer";
+    }
+
+    @GetMapping("/contact")
+    public String email() {
+        return "email";
+    }
+
+    @PostMapping("/save")
+    public String getMessage(MailDto mailDto) {
+        mailService.sendMail(mailDto);
+        return "redirect:success";
+    }
+}
